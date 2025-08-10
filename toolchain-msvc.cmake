3f6d1faa4d1fd7ba6bfe53b393bdfa1ca869609e
Ptilopsi4 <1405469851@qq.com>
2025-08-10 21:04:08 +0800

    完成本地配置

--- toolchain-msvc.cmake
+++ toolchain-msvc.cmake
@@ -4,4 +4,8 @@ set(CMAKE_C_COMPILER "cl.exe")
 set(CMAKE_CXX_COMPILER "cl.exe")
 
 set(CMAKE_SYSTEM_PROCESSOR "x64")
-set(CMAKE_SYSROOT "C:/Program Files (x86)/Windows Kits/10")
+set(CMAKE_SYSROOT "E:/Windows Kits/10")
+
+set(CMAKE_TOOLCHAIN_FILE "E:/DevTools/vcpkg/scripts/buildsystems/vcpkg.cmake" CACHE STRING "Vcpkg toolchain file")
+
+set(Qt6_DIR "E:/Qt/6.6.3/msvc2019_64/lib/cmake/Qt6" CACHE PATH "Path to Qt6 CMake files")
